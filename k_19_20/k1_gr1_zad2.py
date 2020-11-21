fbb9897e24420ec8a83a2d082f53616b3e9f4aab
Mateusz Furga <matfurga@gmail.com>
2020-11-21 11:01:47 +0100

    Update k1_gr1_zad2.py

--- k_19_20/k1_gr1_zad2.py
+++ k_19_20/k1_gr1_zad2.py
@@ -12,7 +12,7 @@ def count_ones(n: int) -> int:
     n //= 2
   return c
 
-def check(t: list, r: int, c1: int, c2: int) -> bool:
+def validate_chunks(t: list, r: int, c1: int, c2: int) -> bool:
   sz = len(t)
   for i in range(sz):
     if i == r:
@@ -24,20 +24,17 @@ def check(t: list, r: int, c1: int, c2: int) -> bool:
         return False
   return True
 
-def main(t):
+def main(t: list):
   sz = len(t)
 
   for i in range(sz):
     for j in range(sz):
-      if count_ones(t[i][j]) % 2 == 1:
-        t[i][j] = 1
-      else:
-        t[i][j] = 0
+      t[i][j] = count_ones(t[i][j]) % 2
  
   for r in range(sz):
     for c1 in range(sz - 1):
       for c2 in range(c1 + 1, sz):
-        if check(t, r, c1, c2):
+        if validate_chunks(t, r, c1, c2):
           return True
 
   return False
